b0e0c761d059703372615194a7af785e080e0660
Voytina <voytinakolya@gmail.com>
2024-04-05 20:45:47 +0300

    home work

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,23 @@
- export function getImages(q) {
-    const BASE_URL = 'https://pixabay.com/api/';
+import axios from 'axios';
 
-    const params = new URLSearchParams({
-        key: '43216493-c3660c641d44e8d68813e69e4',
-        q,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-    })
+export async function getImages(q, p = 1) {
+  const BASE_URL = 'https://pixabay.com/api/';
 
-    const url = `${BASE_URL}?${params}`;
+  const params = new URLSearchParams({
+    key: '43216493-c3660c641d44e8d68813e69e4',
+    q,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    page: p,
+    per_page: 15,
+  });
 
-    return fetch(url)
-    .then(res => res.json())
-    .catch(error => {
-      console.error('Error occurred while fetching images:', error);
-      throw error;
-    });
-
+  const url = `${BASE_URL}?${params}`;
+  try {
+    const response = await axios(url);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { getImages } from './js/pixabay-api';
 import { renderImages } from './js/render-function';
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
@@ -10,51 +10,82 @@ import 'izitoast/dist/css/iziToast.min.css';
 const form = document.querySelector('.form');
 const list = document.querySelector('.list');
 const loader = document.querySelector('.box__loader');
+const buttonLoadMore = document.querySelector('.load-button');
 
-
-
-
-
+let page = 1;
+let value = '';
+let totalImages = 0;
+let counter = 0;
+const reset = () => {
+  page = 1;
+  totalImages = 0;
+  counter = 0;
+};
 const lightbox = new SimpleLightbox('.gallery-link', {
-        captionsData: 'alt',
-        captionsDelay: 250,
-        });
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  loader.classList.toggle('hidden');
+  value = form.elements.search.value.trim();
+  reset();
 
-    e.preventDefault();
-loader.classList.toggle("hidden")
-    const value = form.elements.search.value.trim();
-    if (value === '') {
+  if (value === '') {
     loader.classList.toggle('hidden');
-     return iziToast.warning({message: "Please try again! Write something.", position: "topRight", color: "orange"})
-    }
-    
+    return iziToast.warning({
+      message: 'Please try again! Write something.',
+      position: 'topRight',
+      color: 'orange',
+    });
+  }
 
-    getImages(value)
-        .then(data => {
+  const getPhoto = async () => {
+    try {
+      const data = await getImages(value);
 
-            if (data.hits.length === 0) {
-                iziToast.error({
-                    message: 'Sorry, there are no images matching your search query. Please try again!',
-                    color: 'red',
-                    position: 'topRight',
-                })
-                loader.classList.toggle('hidden');
-                return;
-            }
-
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          color: 'red',
+          position: 'topRight',
+        });
         loader.classList.toggle('hidden');
-        const showApi = renderImages(data.hits);
-        list.innerHTML = showApi;
-        lightbox.refresh();
-        }).catch(error => {
-            loader.classList.toggle("hidden")
-        console.error('Error', error);
-    })
-  
-    e.target.reset()   
+        return;
+      }
 
-})
+      if (data.total > 15) {
+        buttonLoadMore.classList.remove('hidden');
+        totalImages = data.total;
+        counter += 15;
+      }
 
+      loader.classList.toggle('hidden');
+      list.innerHTML = renderImages(data.hits);
+      lightbox.refresh();
+    } catch (error) {
+      console.error(error);
+      loader.classList.toggle('hidden');
+    }
+  };
+  getPhoto();
 
+  e.target.reset();
+});
+
+buttonLoadMore.addEventListener('click', async () => {
+  try {
+    const data = await getImages(value, (page += 1));
+    list.insertAdjacentHTML('beforeend', renderImages(data.hits));
+    counter += 15;
+    lightbox.refresh();
+
+    if (counter > totalImages) {
+      buttonLoadMore.classList.add('hidden');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
